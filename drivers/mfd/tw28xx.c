c9e75bd2ae47cced8aec49d6ba0ec42fb9ab4212
Alexander Shiyan <shc_work@mail.ru>
2017-01-18 13:07:47 +0300

    MFD cont

--- drivers/mfd/tw28xx.c
+++ drivers/mfd/tw28xx.c
@@ -3,31 +3,88 @@
 #include <linux/module.h>
 #include <linux/of_device.h>
 #include <linux/gpio/consumer.h>
+#include <linux/mfd/core.h>
 #include <linux/mfd/tw28xx.h>
 
-#define TW283X_REG_PWDOWN		0x004c
-#define TW283X_REG_AFILTERS		0x0052
-#define TW283X_REG_AFORMAT1		0x0062
-#define TW283X_REG_AFORMAT2		0x006c
-#define TW283X_REG_AMUTE		0x006d
-#define TW283X_REG_DEVICE_ID		0x00fe
-#define TW283X_REG_MEM_INIT		0x017f
-#define TW283X_REG_MASTER_MODE		0x01a4
+static const struct regmap_range tw28xx_reg_ranges[] = {
+	regmap_reg_range(0x000, 0x1bf),
+	regmap_reg_range(0x1ff, 0x2ff),
+};
+
+static const struct regmap_range tw28xx_reg_ro_ranges[] = {
+	regmap_reg_range(0x000, 0x001),
+	regmap_reg_range(0x00d, 0x00d),
+	regmap_reg_range(0x010, 0x011),
+	regmap_reg_range(0x01d, 0x01d),
+	regmap_reg_range(0x020, 0x021),
+	regmap_reg_range(0x02d, 0x02d),
+	regmap_reg_range(0x030, 0x031),
+	regmap_reg_range(0x03d, 0x03d),
+	regmap_reg_range(0x055, 0x055),
+	regmap_reg_range(0x0d8, 0x0d9),
+	regmap_reg_range(0x108, 0x10a),
+	regmap_reg_range(0x15e, 0x15e),
+	regmap_reg_range(0x16e, 0x16f),
+	regmap_reg_range(0x17c, 0x17c),
+	regmap_reg_range(0x18b, 0x18f),
+	regmap_reg_range(0x198, 0x19f),
+	regmap_reg_range(0x29e, 0x29e),
+	regmap_reg_range(0x2be, 0x2be),
+	regmap_reg_range(0x2de, 0x2de),
+	regmap_reg_range(0x2fe, 0x2fe),
+};
+
+static const struct regmap_access_table tw28xx_write_ranges_table = {
+	.yes_ranges	= tw28xx_reg_ranges,
+	.n_yes_ranges	= ARRAY_SIZE(tw28xx_reg_ranges),
+	.no_ranges	= tw28xx_reg_ro_ranges,
+	.n_no_ranges	= ARRAY_SIZE(tw28xx_reg_ro_ranges),
+};
 
 static struct regmap_config tw28xx_regmap_i2c_config = {
 	.reg_bits	= 16,
 	.val_bits	= 8,
-	.max_register	= 0x2ff,
 	.cache_type	= REGCACHE_NONE,
+	.max_register	= 0x2ff,
+	.wr_table	= &tw28xx_write_ranges_table,
 };
 
 static const struct reg_sequence tw28xx_init_patch[] = {
+	/* Recommended values to reserved registers */
+	{ 0x17d, 0x00 },
+	{ 0x1b7, 0x00 },
+	{ 0x1b8, 0x00 },
+	{ 0x1b9, 0x00 },
+	{ 0x1ba, 0x00 },
+	{ 0x1bb, 0x00 },
+	{ 0x1bc, 0x00 },
+	{ 0x1bd, 0x00 },
+	{ 0x1be, 0x00 },
+	{ 0x1bf, 0x00 },
 	/* Initialize the operation mode of SDRAM */
-	{ TW283X_REG_MEM_INIT, (1 << 7) },
+	{ 0x17f, BIT(7) },
 	/* Master operation mode */
-	{ TW283X_REG_MASTER_MODE, (1 << 7) },
-	/* Power down the audio DAC */
-	{ TW283X_REG_PWDOWN, (1 << 5) },
+	{ 0x1a4, BIT(7) },
+	/* Power down Audio DAC, Audio ADC, Video ADCs */
+	{ 0x04c, 0x3f },
+	/* Power down Video DACs */
+	{ 0x1a1, BIT(3) | BIT(7) },
+	{ 0x1a2, BIT(3) },
+};
+
+static void tw28xx_shutdown(struct tw28xx *tw28xx)
+{
+	clk_disable_unprepare(tw28xx->clk);
+
+	if (!IS_ERR(tw28xx->vddo))
+		regulator_disable(tw28xx->vddo);
+	if (!IS_ERR(tw28xx->vddi))
+		regulator_disable(tw28xx->vddi);
+}
+
+static const struct mfd_cell tw28xx_devs[] = {
+	{ .name = "tw28xx-audio", },
+	{ .name = "tw28xx-video", },
 };
 
 static int tw28xx_probe(struct i2c_client *client,
@@ -88,9 +145,18 @@ static int tw28xx_probe(struct i2c_client *client,
 
 	tw28xx->irq = client->irq;
 
-	ret = regmap_read(tw28xx->regmap, TW283X_REG_DEVICE_ID, &data);
+	ret = regmap_multi_reg_write(tw28xx->regmap, tw28xx_init_patch,
+				     ARRAY_SIZE(tw28xx_init_patch));
+	if (ret) {
+		tw28xx_shutdown(tw28xx);
+		return ret;
+	}
+
+	/* Get Device ID */
+	ret = regmap_read(tw28xx->regmap, 0x0fe, &data);
 	if (ret) {
 		dev_err(&client->dev, "Regmap read error.\n");
+		tw28xx_shutdown(tw28xx);
 		return -ENODEV;
 	}
 
@@ -105,40 +171,22 @@ static int tw28xx_probe(struct i2c_client *client,
 		break;
 	default:
 		dev_err(&client->dev, "No TW28xx found (0x%02x).\n", data);
+		tw28xx_shutdown(tw28xx);
 		return -ENODEV;
 	}
 
-	ret = regmap_multi_reg_write(tw28xx->regmap, tw28xx_init_patch,
-				     ARRAY_SIZE(tw28xx_init_patch));
-
-	/* Initialize the operation mode of SDRAM */
-//	regmap_write(tw28xx->regmap, TW283X_REG_MEM_INIT, (1 << 7));
-	/* Master operation mode */
-//	regmap_write(tw28xx->regmap, TW283X_REG_MASTER_MODE, (1 << 7));
-	/* Power down the audio DAC */
-//	regmap_write(tw28xx->regmap, TW283X_REG_PWDOWN, (1 << 5));
-	/* Analog Audio input Anti-Aliasing Filter enabled */
-//	regmap_write(tw28xx->regmap, TW283X_REG_AFILTERS, (1 << 4));
-	/* I2S, 4 audios */
-//	regmap_write(tw28xx->regmap, TW283X_REG_AFORMAT1, (1 << 0));
-	/* I2S, Audio ADC Function mode, Master */
-//	regmap_write(tw28xx->regmap, TW283X_REG_AFORMAT2, (1 << 6) | (1 << 5) | (1 << 0));
-	/* Mute Playback audio input */
-//	regmap_write(tw28xx->regmap, TW283X_REG_AMUTE, (1 << 4));
+	ret = devm_mfd_add_devices(&client->dev, PLATFORM_DEVID_AUTO,
+				   tw28xx_devs, ARRAY_SIZE(tw28xx_devs),
+				   NULL, 0, NULL);
+	if (ret)
+		tw28xx_shutdown(tw28xx);
 
 	return ret;
 }
 
 static int tw28xx_remove(struct i2c_client *client)
 {
-	struct tw28xx *tw28xx = dev_get_drvdata(&client->dev);
-
-	if (!IS_ERR(tw28xx->vddo))
-		regulator_disable(tw28xx->vddo);
-	if (!IS_ERR(tw28xx->vddi))
-		regulator_disable(tw28xx->vddi);
-
-	clk_disable_unprepare(tw28xx->clk);
+	tw28xx_shutdown(dev_get_drvdata(&client->dev));
 
 	return 0;
 }
@@ -168,6 +216,6 @@ static struct i2c_driver tw28xx_driver = {
 };
 module_i2c_driver(tw28xx_driver);
 
-MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Alexander Shiyan <shc_work@mail.ru>");
-MODULE_DESCRIPTION("Techwell/Intersil TW28xx driver");
+MODULE_DESCRIPTION("Techwell/Intersil TW28xx MFD driver");
+MODULE_LICENSE("GPL");
