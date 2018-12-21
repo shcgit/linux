ec7ebe4561f88153780f7c49e506ead396f3bcc5
Alexander Shiyan <shc_work@mail.ru>
2018-12-21 11:57:55 +0300

    Fix imx51 reboot

--- arch/arm/mach-imx/mach-imx51.c
+++ arch/arm/mach-imx/mach-imx51.c
@@ -100,5 +100,4 @@ DT_MACHINE_START(IMX51_DT, "Freescale i.MX51 (Device Tree Support)")
 	.init_machine	= imx51_dt_init,
 	.init_late	= imx51_init_late,
 	.dt_compat	= imx51_dt_board_compat,
-	.restart	= mxc_restart,
 MACHINE_END
