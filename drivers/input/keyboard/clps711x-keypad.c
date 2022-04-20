84b6ed4cd05a8bd1fbb250112b3f7d1cbac438fd
Alexander Shiyan <eagle.alexander923@gmail.com>
2022-04-20 09:30:35 +0300

    Cleanup clps711x-keypad

--- drivers/input/keyboard/clps711x-keypad.c
+++ drivers/input/keyboard/clps711x-keypad.c
@@ -96,14 +96,6 @@ static int clps711x_keypad_probe(struct platform_device *pdev)
 		return -ENOMEM;
 
 	priv->syscon = syscon_regmap_lookup_by_phandle(np, "syscon");
-	if (IS_ERR(priv->syscon) && (PTR_ERR(priv->syscon) != -EPROBE_DEFER)) {
-		/* falling back to old bindings */
-		priv->syscon =
-			syscon_regmap_lookup_by_compatible("cirrus,ep7209-syscon1");
-		if (!IS_ERR(priv->syscon))
-			dev_warn(dev, "Please migrate driver bindings"
-				      " to use syscon phandle.\n");
-	}
 	if (IS_ERR(priv->syscon))
 		return PTR_ERR(priv->syscon);
 
